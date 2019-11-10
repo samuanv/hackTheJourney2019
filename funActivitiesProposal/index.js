f91a23d157d929606733b3d439bb159413ce54ca
Samuel Andreo <samuel.andreo.vilaplana@ibm.com>
2019-11-10 10:22:49 +0000

    Trip Purpose

--- funActivitiesProposal/index.js
+++ funActivitiesProposal/index.js
@@ -17,16 +17,26 @@ module.exports = async function (context, req) {
             longitude : long
         })
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
+        const getTripPurpose = async (originAirportCode, destinationAirportCode) => amadeus.travel.predictions.tripPurpose.get({
+            originLocationCode: originAirportCode || 'NYC',
+            destinationLocationCode: destinationAirportCode || 'MAD',
+            departureDate: '2020-08-01',
+            returnDate: '2020-08-12'
+        })
+    try {
+        const {data: pointsOfInterest} = await getPointOfInterests(...BCN_Coords);
+        const {data: tripPurpose} = await getTripPurpose();
         context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            body:{
+                pointsOfInterest,
+                tripPurpose
+            }
+        };
+    }catch(e) {
+        context.res = {
+            status: 400,
+            body: e
         };
     }
 };
