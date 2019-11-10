d0b9f9cc333e8c72cf3063fbe4dd1df3ecbb4d17
Samuel Andreo <samuel.andreo.vilaplana@ibm.com>
2019-11-10 11:51:38 +0000

    Fix package

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+# Configure storage to host static apps
+# az storage blob service-properties update --account-name hackthejourneyfrontend --static-website --404-document index.html --index-document index.html
+
+
+az storage blob upload-batch -s build -d \$web --account-name hackthejourneyfrontend
+# Show link
+az storage account show -n hackthejourneyfrontend -g hackthejourney --query "primaryEndpoints.web" --output tsv
