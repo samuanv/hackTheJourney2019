2e055855713bc3929345d6c2a645843cfb39b10a
Samuel Andreo <samuel.andreo.vilaplana@ibm.com>
2019-11-10 11:54:56 +0000

    Deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 # Configure storage to host static apps
 # az storage blob service-properties update --account-name hackthejourneyfrontend --static-website --404-document index.html --index-document index.html
+cd app
 
+npm run build
 
 az storage blob upload-batch -s build -d \$web --account-name hackthejourneyfrontend
 # Show link
