031b466cdc456fd4abeb439fdc27a6242e2b194e
Samuel Andreo <samuel.andreo.vilaplana@ibm.com>
2019-11-10 11:47:42 +0000

    CouchBase Integration

--- funActivitiesProposal/index.js
+++ funActivitiesProposal/index.js
@@ -1,6 +1,8 @@
 const Amadeus = require('amadeus');
-
+const axios = require('axios')
 const BCN_Coords = [41.397158, 2.160873]
+
+const couchbase = require('couchbase')
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
@@ -11,6 +13,10 @@ module.exports = async function (context, req) {
         clientSecret: 'iKAwssAThsAKw3vO',
         logLevel: 'debug'
       });
+
+    const cluster = new couchbase.Cluster('couchbase://ec2-3-95-8-255.compute-1.amazonaws.com');
+    cluster.authenticate('Administrator', 'Couchbase');
+    const bucket = cluster.openBucket('travel-sample');
 
     const getPointOfInterests = async (lat, long) => amadeus.referenceData.locations.pointsOfInterest.get({
             latitude : lat,
@@ -23,14 +29,22 @@ module.exports = async function (context, req) {
             departureDate: '2020-08-01',
             returnDate: '2020-08-12'
         })
+
+        const getActivitiesFormCity = (cityCode) => bucket.get(cityCode, () => {
+
+        });
+
     try {
         const {data: pointsOfInterest} = await getPointOfInterests(...BCN_Coords);
         const {data: tripPurpose} = await getTripPurpose();
+        // TODO: Promisify
+        // const activities = getActivitiesFormCity();
         context.res = {
             status: 400,
             body:{
                 pointsOfInterest,
-                tripPurpose
+                tripPurpose,
+                activities
             }
         };
     }catch(e) {
@@ -39,4 +53,5 @@ module.exports = async function (context, req) {
             body: e
         };
     }
-};
+    }
+
